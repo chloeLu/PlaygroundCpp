589639d27145ae85672d99a33e9a9a57a355325a
xd Lu <eleanor.lxd@gmail.com>
2015-03-19 21:57:14 +0800

    SSS Infinite

--- src/SSS_Infinite.cpp
+++ src/SSS_Infinite.cpp
@@ -5,7 +5,6 @@
 // Copyright   : 
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-#include "PlaygroundCpp.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -51,10 +50,10 @@ void readData(string infileStr, vector<vector<int> >& data) {
 	}
 }
 
-vector<map<int, string>* > constructRenamingMap(vector<vector<int> > data) {
+vector<map<int, string>*> constructRenamingMap(vector<vector<int> > data) {
 
 	// create and init the map
-	vector<map<int, string>* > rrm ;
+	vector<map<int, string>*> rrm;
 	for (unsigned i = 0; i < data.size(); i++) {
 		map<int, string>* aMap = new map<int, string>();
 		rrm.push_back(aMap);
@@ -84,9 +83,69 @@ vector<map<int, string>* > constructRenamingMap(vector<vector<int> > data) {
 	return rrm;
 }
 
-unsigned int run(vector<vector<int> > instructions, vector<map<int, string>* > rrm) {
+map<int, set<int>*> constructDependencyMap(vector<vector<int> > data) {
+
+	// create and init the map
+	map<int, set<int>*> dMap;
+	for (unsigned i = 0; i < data.size(); i++) {
+		set<int>* v = new set<int>();
+		dMap[i] = v;
+	}
+
+	for (unsigned i = 0; i < data.size(); i++) {
+		// detect RAW dependencies on following instructions
+		int dest = data[i][0];
+		int latency = data[i][3];
+		for (unsigned j = i + 1; (j <= i + latency && j < data.size()); j++) {
+			if (dest == data[j][1] || dest == data[j][2]) {
+				dMap[j]->insert(i);
+			}
+		}
+	}
+
+	return dMap;
+}
+
+unsigned int run(map<int, set<int>*> dMap, vector<vector<int> > instr) {
 	unsigned int timeElapsed = 0;
-	map<int, set<int> > dependencyMap;;
+	while (!dMap.empty()) {
+		// find instructions to execute
+		set<int> instrToExecute;
+		for (map<int, set<int>*>::iterator it = dMap.begin(); it != dMap.end(); it++) {
+			int instrId = it->first;
+			if (it->second->empty()) {
+				instrToExecute.insert(instrId);
+//				cout << "cycle #" << timeElapsed << "; executing instr #" << instrId << endl;
+//				cout << instr.at(instrId)[0] << " " << instr.at(instrId)[1] << " " << instr.at(instrId)[2] << " "
+//						<< instr.at(instrId)[3] << " " << endl;
+			}
+		}
+
+		// execute
+		for (int instrId : instrToExecute) {
+			if (instr.at(instrId)[3] > 1) {
+				instr.at(instrId)[3]--;
+			} else {
+				dMap.erase(dMap.find(instrId));
+			}
+		}
+
+		//update dependencies in dMap
+		for (map<int, set<int>*>::iterator it = dMap.begin(); it != dMap.end(); it++) {
+			set<int>* dependency = it->second;
+			for (set<int>::iterator setIt = dependency->begin(); setIt != dependency->end();) {
+				if (dMap.find(*setIt) == dMap.end()) {
+					dependency->erase(setIt++);
+				} else {
+					++setIt;
+				}
+			}
+		}
+
+		timeElapsed++;
+//		cout << "time elapsed:" << timeElapsed << endl;
+	}
+	return timeElapsed;
 }
 
 int main(int argc, char *argv[]) {
@@ -98,15 +157,20 @@ int main(int argc, char *argv[]) {
 	vector<vector<int> > instructions;
 	readData(infileStr, instructions);
 
-	vector<map<int, string>* > rrm = constructRenamingMap(instructions);
+//	vector<map<int, string>* > rrm = constructRenamingMap(instructions);
+//
+//	int count = 1;
+//	for (map<int, string>* aMap : rrm) {
+//		cout << count++ << endl;
+//		for (map<int, string>::iterator iterator = aMap->begin(); iterator != aMap->end(); iterator++) {
+//			cout << "Register:" << iterator->first << "; Label:" << iterator->second << endl;
+//		}
+//	}
 
-	int count = 1;
-	for (map<int, string>* aMap : rrm) {
-		cout << count++ << endl;
-		for (map<int, string>::iterator iterator = aMap->begin(); iterator != aMap->end(); iterator++) {
-			cout << "Register:" << iterator->first << "; Label:" << iterator->second << endl;
-		}
-	}
+	map<int, set<int>*> dMap = constructDependencyMap(instructions);
+
+	cout << "# cycles needed:" << endl;
+	cout << run(dMap, instructions) << endl;
 
 	return 0;
 }
